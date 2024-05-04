d30fe59c2f746d306d94d491924f383f815080a9
Gustavo Alves <medeirosgustavo.a@gmail.com>
2024-05-04 00:55:14 -0300

    estudo de operador ternário com diferentes tipos

--- tipos-variaveis/src/TiposVariaveis/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis/TiposVariaveis.java
@@ -2,6 +2,21 @@ package TiposVariaveis;
 
 public class TiposVariaveis {
     public static void main(String[] args) {
+        // byte idade = 20;
+        // short ano = 2024;
+        // int cep = 25487256;
+        // long cpf = 58981568777L;
+        // float pi = 3.14F;
+        // double salario = 2500.52;
+
+        int a = 5;
+        int b = 5;
+
+        String res = a==b ? "verdadeiro" : "falso";
+        int res2 = a==b ? 1 : 0;
         
+        System.out.println(res);
+        System.out.println(res2);
+
     }
 }
